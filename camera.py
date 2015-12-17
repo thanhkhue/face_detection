206d70ae6ab4f69d43d3f4f59bc87edbc7efefdf
thanhkhue <thanhkhuebkdn2012@gmail.com>
2015-12-17 19:21:39 +0700

    face detection

--- camera.py
+++ camera.py
@@ -13,10 +13,45 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
     
+
+
+    try:
+        cascPath = argv[1]
+    except Exception:
+        cascPath = "haarcascade_frontalface_default.xml"
+
     def get_frame(self):
-        success, image = self.video.read()
+        #  success, image = self.video.read()
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
         # video stream.
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        
+
+        
+        faceCascade = cv2.CascadeClassifier(VideoCamera.cascPath)
+
+        # Capture frame-by-frame
+        ret, frame = self.video.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags =  cv2.CASCADE_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        #todo just cast jpeg to
+        file = open("binary.file","wb")
+        file.write(jpeg)
+        file.close()
+        jpeg = open("binary.file","rb").read()
+        return jpeg
